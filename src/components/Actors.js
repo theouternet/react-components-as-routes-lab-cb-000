21226ea00206b8b26a60af6d0c473868c8463d70
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-02 03:14:39 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,8 +4,8 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+  <h1>Actors Page</h1>
+      {actors.map(actor => <div className="actor"><h2>{actor.name}</h2><p>Movies:<ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul></p></div>)}    </div>
   );
 };
 
